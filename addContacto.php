479969216ed5a85aa56077cd3cd1c64a92cdce46
Guiomar Cameras <ada_hack243@hackademy.mx>
2022-11-08 17:58:56 -0600

    código php

---
+++ addContacto.php
@@ -0,0 +1,11 @@
+<?php
+    /* Se crean variables con el signo de dolar */
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    echo($nombre);
+    echo($correo);
+    echo($mensaje);
+
+?>
